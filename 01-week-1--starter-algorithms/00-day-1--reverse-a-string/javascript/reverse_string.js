1719719abcf22ae6c64519fa4640af8008d9c8e2
Naomi-Rhames <naomirhames2020@gmail.com>
2022-01-07 21:07:28 -0600

    behind my thinking

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,5 +1,5 @@
 function reverseString(str) {
-  // type your code here
+ return [...str].reverse().join('')
 }
 
 if (require.main === module) {
@@ -16,4 +16,12 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+// we want whatever the string inside the argument in reverse
+ //  My thoughts: .revrse() reverses the string to cause the string to go into revrese order
+
+
+
 // And a written explanation of your solution
+// My thoughts were since I am dealing with methods that usally works only with arrays I assumed that the arrgument was filled with array of strings.
+// So since I know you never want to tamper with the original array, you would have to copy the array
+// Once you copy it then you want to apply the .reverse() method to revrse the string and then have the .join() method to join the words togethor
