3582e68daece6db3690cfe1feb69743542ee13ec
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-04-29 04:54:07 -0600

    definitely not scared of recursion

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -1,5 +1,10 @@
 function recursiveCount(num = 0) {
-  // type your code here
+  if (num <= 9) {
+  console.log(num)
+  
+
+  recursiveCount(num + 1)
+  }
 }
 
 if (require.main === module) {
@@ -11,3 +16,7 @@ module.exports = recursiveCount;
 // OPTIONAL
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//I make an if statement to establish the base count.
+//I then console.log(num) to print num as it returns up the stack
+//I make the recursive call of recursiveCount(num + 1)
