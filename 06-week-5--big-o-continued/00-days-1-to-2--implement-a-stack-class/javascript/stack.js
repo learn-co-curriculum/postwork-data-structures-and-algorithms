4fa23e36eacb2df1fd26ba3803022fccdf1f334a
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-04-30 02:00:20 -0600

    Getting better?

--- 06-week-5--big-o-continued/00-days-1-to-2--implement-a-stack-class/javascript/stack.js
+++ 06-week-5--big-o-continued/00-days-1-to-2--implement-a-stack-class/javascript/stack.js
@@ -9,45 +9,57 @@ class Stack {
   // add item to top of stack if not full
   // if full throw error
   push(item) {
-
+    if (!this.isFull()) {
+      this.stack.push(item)
+    } else {
+      throw new Error("Stack is full!")
+    }
   }
 
   // remove item from top of stack and return it
   pop() {
-
+    return this.stack.pop()
   }
 
   // return item at top of stack without removing it
   peek() {
-
+    return this.stack[this.size() -1]
   }
 
   // return true if stack is empty, otherwise false
   isEmpty() {
-
+   return this.size() === 0
   }
 
   // return true if stack is full, otherwise false
   isFull() {
-
+    return this.size() === this.limit
   }
 
   // return number of items in stack
   size() {
-
+    return this.stack.length
   }
 
   // return -1 if item not in stack, otherwise integer representing 
   // how far it is from the top
   search(target) {
+    for (let i = -1; i >= -this.size(); --i) {
+      if (this.stack[this.size() + i] === target) {
+        return Math.abs(i) - 1;
+      }
+    }
 
+    return -1;
   }
+
 
   // print contents of stack: do not return the stack itself!
   print() {
-    
+    console.log(this.stack.join(' <- '))
   }
 }
+
 
 if (require.main === module) {
   // add your own tests in here
