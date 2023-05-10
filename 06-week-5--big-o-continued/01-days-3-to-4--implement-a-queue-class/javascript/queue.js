288e34bc1c24a99716ad354b62a580f0024d929f
Kyle Wehrung <kyle.wehrung@gmail.com>
2023-05-10 16:49:04 -0600

    Queuesgit add .

--- 06-week-5--big-o-continued/01-days-3-to-4--implement-a-queue-class/javascript/queue.js
+++ 06-week-5--big-o-continued/01-days-3-to-4--implement-a-queue-class/javascript/queue.js
@@ -9,43 +9,59 @@ class Queue {
   // add item to rear of queue if not full
   // if full throw error
   enqueue(item) {
+    if (!this.isFull()) {
+      this.queue.push(item)
+    } else {
+      throw new Error("Queue is full!")
+    }
 
   }
+
 
   // remove item from front of queue and return it
   dequeue() {
-
+    return this.queue.shift()
   }
+
 
   // return item at front of queue without removing it
   peek() {
-
+    return this.queue[0]
   }
 
   // return true if queue is empty, otherwise false
   isEmpty() {
-
+    if (this.size() === 0) {
+      return true
+    } else {
+      return false
+    }
   }
 
   // return true if queue is full, otherwise false
   isFull() {
-
+    return this.size() === this.limit
   }
 
   // return number of items in queue
   size() {
-
+    return this.queue.length
   }
 
   // return -1 if item not in queue, otherwise integer representing 
   // how far it is from the front
   search(target) {
-
+    if( this.queue.indexOf(target) > -1) {
+      return this.queue.indexOf(target)
+    } else {
+      return -1
+    }
+    
   }
 
   // print contents of queue: do not return the queue itself!
   print() {
-    
+    console.log(this.queue.join(' <- '))
   }
 }
 
